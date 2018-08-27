2e71cd5cb0f4975c798ba79d6fed5f8d937d6f4f
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-27 19:24:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ function moveDodgerLeft() {
     const left = positionToInteger(DODGER.style.left)
     
     if (left > 0) {
-      DODGER.style.left = `${left - 4}px`
+      DODGER.style.left = `${left - 4}px`;
     }
   }
 }
