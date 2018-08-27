05467cc7fa807e82d8245a1ca1681df778f84386
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-27 19:38:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
+     if checkCollision(rock)
 
     /**
      * Otherwise, if the rock hasn't reached the bottom of
