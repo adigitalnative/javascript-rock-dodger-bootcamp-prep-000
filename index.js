c0c92f4f3cfb6a360a61f567280a43d5e4caebd7
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-27 19:35:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,20 +31,16 @@ function checkCollision(rock) {
     const rockLeftEdge = positionToInteger(rock.style.left)
     const rockRightEdge = positionToInteger(rock.style.left) + 20;
 
-    if (rockLeftEdge < dodgerLeftEdge &&)
-
-    if (false /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge
-               */) {
+    if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) {
       return true
+    } else if (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) {
+      return true
+    } else if (rockLeftEdge <= dodgerRightEdge && rockRightEdge >- dodgerRightEdge) {
+      return true
+    } else {
+      return false
     }
+
   }
 }
 
